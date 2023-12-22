422a4d68950187bc0b2d6a399aa2fa1ccd1db25a
lost0817 <hwenyang920817@gmail.com>
2023-12-22 11:06:37 +0800

    1222

--- HW_Server.js
+++ HW_Server.js
@@ -2,22 +2,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const HW_Server = express();
-const port = 8170;
+const app = express();
+const port = 3000;
 
-// 使用 body-parser 中介軟體來解析 POST 請求的主體
-HW_Server.use(bodyParser.urlencoded({ extended: true }));
-HW_Server.use(bodyParser.json());
-
-// 設定靜態檔案路徑
-//HW_Server.use(express.static(path.join(__dirname, 'web-class')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // 設定根路由，回傳您製作好的 HTML 檔案
-HW_Server.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'web-class', 'HW03.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'HW03.html'));
 });
 
 //啟動 Express 伺服器
-HW_Server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
